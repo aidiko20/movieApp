6620af983328d41bea363f5e147feafd44b865e7
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-22 22:59:26 -0500

    added more styling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const APIURL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+const main = document.querySelector("main");
+
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
@@ -17,14 +19,24 @@ async function getMovies() {
             <img src="${IMGPATH + poster_path}" alt="${title}">
             <div class="movie-info">
             <h3>${title}</h3>
-            <span>${vote_average}</span>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         `;
 
-        document.body.appendChild(movieEl);
+        main.appendChild(movieEl);
     });
 
     return respData;
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+    return "orange";
+    } else {
+        return "red";
+    }
 }
 
 getMovies();
