715b60bdebe7edc8d7d46911743e71da3cd040d2
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-23 22:12:34 -0500

    added search function

--- script.js
+++ script.js
@@ -2,16 +2,28 @@ const APIURL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-const main = document.querySelector("main");
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-async function getMovies() {
-    const resp = await fetch(APIURL);
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
-    console.log(respData);
+    showMovies(respData.results);
+}
 
-    respData.results.forEach((movie) => {
-        const  { poster_path, title, vote_average } = movie;
+function showMovies(movies) {
+    //clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average } = movie;
+
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
 
@@ -20,23 +32,33 @@ async function getMovies() {
             <div class="movie-info">
             <h3>${title}</h3>
             <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        `;
+            </div>
+            `;
 
         main.appendChild(movieEl);
     });
-
-    return respData;
 }
 
+
 function getClassByRate(vote) {
-    if(vote >= 8) {
+    if (vote >= 8) {
         return "green";
     } else if (vote >= 5) {
-    return "orange";
+        return "orange";
     } else {
         return "red";
     }
 }
 
-getMovies();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
+
